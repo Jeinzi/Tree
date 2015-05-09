6c0589d07a735ed2fb8055819bfc8cdc3fb8c168
Jeinzi <Jeinzi@gmx.de>
2015-05-10 01:55:03 +0200

    Updated variable names.

--- Tree/main.cpp
+++ Tree/main.cpp
@@ -4,15 +4,16 @@
 #include <fstream>
 #include <conio.h>
 #include <Shlwapi.h>
+#include <stdlib.h>
 #include "General.h"
 #include "Progress.h"
 #pragma comment(lib, "shlwapi.lib")
 
 #ifdef DEBUG
-#define VERSION "1.31d"
+	#define VERSION "1.32d"
 #endif
 #ifdef RELEASE
-#define VERSION "1.31r"
+	#define VERSION "1.32r"
 #endif
 
 using namespace std;
@@ -20,8 +21,8 @@ using namespace std;
 int main()
 {
 	// Initializing console.
-	string Input = "";
-	string Command = "";
+	string input = "";
+	string command = "";
 	PrepareConsole("Tree", VERSION, "Please name the file which should be source of the tree.\nType \"help\" for further information.");
 
 	// Loop waits for correct input.
@@ -29,17 +30,17 @@ int main()
 	{
 		Color(INPUT_COLOR);
 		cout << "Tree.exe> ";
-		getline(cin, Input, '\n');
-		Command = ToLower(GetWord(Input, 0));
+		getline(cin, input, '\n');
+		command = ToLower(GetWord(input, 1));
 		Color(OUTPUT_COLOR);
 
 		// Parsing.
-		if (Command == "exit" || Command == "quit")
+		if (command == "exit" || command == "quit")
 		{
 			// Leaving program when "exit" or "quit" has been typed.
-			return(0);
+			return(EXIT_SUCCESS);
 		}
-		else if (Command == "help")
+		else if (command == "help")
 		{
 			// Shows a description of all available commands.
 			cout << "   Available Commands:" << endl;
@@ -51,136 +52,136 @@ int main()
 			cout << "   The program will create the listed files and directories," << endl;
 			cout << "   until you press 'q' or the file reaches its end." << endl << endl;
 		}
-		else if (Command == "info")
+		else if (command == "info")
 		{
 			// Shows general info about the software.
 			cout << "   Tree Version " << VERSION << endl;
 			cout << "   Compiled: " << __DATE__ << ", " << __TIME__ << endl;
-			cout << "   (c) 2012 - 2014 Julian Heinzel" << endl << endl;
+			cout << "   (c) 2012 - 2015 Julian Heinzel" << endl << endl;
 		}
-		else if (Command == "clear" || Command == "cls")
+		else if (command == "clear" || command == "cls")
 		{
 			// Resets the terminal.
 			PrepareConsole("Tree", VERSION, "Please name the file which should be source of the tree.\nType \"help\" for further information.");
 		}
-		else if (!PathFileExists(Input.c_str()))
+		else if (!PathFileExists(input.c_str()))
 		{
 			// Printing an error, if no command and no valid path has been typed.
-			cout << "   Did not find file \"" + Input + "\"." << endl << endl;
+			cout << "   Did not find file \"" + input + "\"." << endl << endl;
 		}
 		else
 		{
 			// Restoring the directory and file structure.
-			string		Line;
-			ifstream	FileIn;
-			Progress	ProgressBar;
+			string		line;
+			ifstream	fileIn;
+			Progress	progressBar;
 
 			// Counting the total number of lines in the specified textfile.
-			int NumberLines = 0;
+			int numberOfLines = 0;
 			cout << "   File found, counting lines..." << endl;
-			FileIn.open(Input);
-			while (getline(FileIn, Line))
+			fileIn.open(input);
+			while (getline(fileIn, line))
 			{
-				NumberLines += 1;
+				numberOfLines += 1;
 			}
-			ProgressBar.Reset(NumberLines, 3);
+			progressBar.Reset(numberOfLines, 3);
 
 			// Opening file and skipping the first three lines.
-			cout << "   Processing " << NumberLines << " lines of text." << endl << "   ";
-			FileIn.close();
-			FileIn.open(Input);
-			getline(FileIn, Line);
-			getline(FileIn, Line);
-			getline(FileIn, Line);
+			cout << "   Processing " << numberOfLines << " lines of text." << endl << "   ";
+			fileIn.close();
+			fileIn.open(input);
+			getline(fileIn, line);
+			getline(fileIn, line);
+			getline(fileIn, line);
 
 			// Creating directories and files.
-			string Pfad[100];
-			Pfad[0] = "C\\";
+			string path[100];
+			path[0] = "C\\";
 			if (!PathFileExists("C\\"))
 			{
 				CreateDirectory("C\\", 0);
 			}
 
 			// Loops through every line in the file.
-			while (getline(FileIn, Line))
+			while (getline(fileIn, line))
 			{
-				bool	Start = true;
-				int		Level = 0;
-				int		Length = strlen(Line.c_str());
-				int		Space = 0;
-				string	NewName = "";
+				bool	start = true;
+				int		level = 0;
+				int		length = strlen(line.c_str());
+				int		space = 0;
+				string	newName = "";
 
 				// Update the terminal, quit on 'q' and 'e'
-				ProgressBar.Increment();
-				ProgressBar.Print();
+				progressBar.Increment();
+				progressBar.Print();
 				if (_kbhit())
 				{
 					char C = _getch();
 					if (C == 'q' || C == 'e')
 					{
-						cout << endl << "   Program canceled. Processed " << ProgressBar.GetCounter() << " of " << ProgressBar.GetMaxElements() << " lines. (" << ProgressBar.GetString() << ")" << endl << endl;
+						cout << endl << "   Program canceled. Processed " << progressBar.GetCounter() << " of " << progressBar.GetMaxElements() << " lines. (" << progressBar.GetString() << ")" << endl << endl;
 						break;
 					}
 				}
 
-				if (Length == 0)
+				if (length == 0)
 				{
 					break;
 				}
 
 				// Extract directory/file name from line.
-				for (int i = 0; i < Length; i++)
+				for (int i = 0; i < length; i++)
 				{
-					char Character = Line[i];
-					if ((Character != ' ') && (Character != '-') && (Character != '\\') && (Character != '|') && (Character != '+') && (Start = true))
+					char c = line[i];
+					if ((c != ' ') && (c != '-') && (c != '\\') && (c != '|') && (c != '+') && (start = true))
 					{
-						Start = false;
+						start = false;
 					}
-					if (Start == true)
+					if (start == true)
 					{
-						Space++;
+						space++;
 					}
-					if (Start == false)
+					if (start == false)
 					{
-						NewName += Character;
+						newName += c;
 					}
 				}
-				Start = true;
-				Level = ((Space / 4) - 1);
+				start = true;
+				level = ((space / 4) - 1);
 
 				// If there is no valid file or directory name, the next line is beeing evaluated.
-				if (NewName == "")
+				if (newName == "")
 				{
 					continue;
 				}
 
 				// If there is a '+' or a '\' in the given line, the name will be interpreted as a directory.
 				// The new path is saved and the directory created.
-				if ((Line.find('+') != -1) || (Line.find("\\") != -1))
+				if ((line.find('+') != -1) || (line.find("\\") != -1))
 				{
-					Level += 1;
-					Pfad[Level] = Pfad[Level - 1] + "\\" + NewName;
-					if (PathFileExists(Pfad[Level - 1].c_str()))
+					level += 1;
+					path[level] = path[level - 1] + "\\" + newName;
+					if (PathFileExists(path[level - 1].c_str()))
 					{
-						if (!PathFileExists(Pfad[Level].c_str()))
+						if (!PathFileExists(path[level].c_str()))
 						{
-							CreateDirectory(Pfad[Level].c_str(), 0);
+							CreateDirectory(path[level].c_str(), 0);
 						}
 					}
 				}
 				else
 				{
 					// Otherwise a file is listed, which will then be created.
-					ofstream FileOut;
-					FileOut.open(Pfad[Level] + "\\" + NewName);
-					FileOut.close();
+					ofstream fileOut;
+					fileOut.open(path[level] + "\\" + newName);
+					fileOut.close();
 				}
 			}
 
-			if (ProgressBar.GetPercent() == 100)
+			if (progressBar.GetPercent() == 100)
 			{
 				// Info if the file has been successfully processed.
-				cout << endl << "   Generated files from \"" + Input + "\" succesfully." << endl << endl;
+				cout << endl << "   Generated files from \"" + input + "\" succesfully." << endl << endl;
 			}
 		}
 	}
